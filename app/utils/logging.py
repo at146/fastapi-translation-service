26c9344e0ed0a4aeffd94ff5bf455f022510527c
Alex Tarasov <at146@ya.ru>
2025-10-06 22:09:12 +0300

    feat: add log
    Signed-off-by: Alex Tarasov <at146@ya.ru>

---
+++ app/utils/logging.py
@@ -0,0 +1,53 @@
+import logging
+import pathlib
+from logging.config import dictConfig
+
+LOGGER_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "basic": {
+            "class": "logging.Formatter",
+            "datefmt": "%d-%m-%Y %H:%M:%S",
+            "format": "%(asctime)s (%(name)s:%(filename)s:%(lineno)d) %(levelname)s - %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "basic",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "basic",
+            "filename": "logs/log.log",
+            "when": "midnight",
+            "backupCount": 31,
+            "encoding": "utf-8",
+        },
+        "error_file": {
+            "class": "logging.FileHandler",
+            "level": "WARNING",
+            "formatter": "basic",
+            "filename": "logs/error_log.log",
+            "mode": "a",
+            "encoding": "utf-8",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["file", "error_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+
+def setup_logger() -> logging.Logger:
+    pathlib.Path("logs").mkdir(parents=True, exist_ok=True)
+    dictConfig(LOGGER_CONFIG)
+    return logging.getLogger(__name__)
